109f41e94cbd88bd20ead4571c7e1bad08ebeff0
Matthammond96 <Matt@Matt.local>
2016-02-29 17:29:30 +0000

    360 image view

--- 360 Image Viewer/ViewController.swift
+++ 360 Image Viewer/ViewController.swift
@@ -1,25 +1,50 @@
-//
-//  ViewController.swift
-//  360 Image Viewer
-//
-//  Created by Matthew Hammond on 19/02/2016.
-//  Copyright © 2016 Matthew Hammond. All rights reserved.
-//
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var image: UIImageView!
+     let motionManager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        if motionManager.gyroAvailable {
+            
+            print(motionManager.gyroAvailable)
+        
+            motionManager.deviceMotionUpdateInterval = 0.1
+            motionManager.gyroUpdateInterval = 0.1
+            
+            motionManager.startDeviceMotionUpdates()
+            motionManager.startGyroUpdates()
+            
+            motionManager.startDeviceMotionUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: { data, error in
+                //print(data?.gravity.x)
+                //print(data?.gravity.y)
+                //let rotation = atan2((data?.rotationRate.x)!, (data?.rotationRate.y)!) - M_PI
+                //self.image.transform = CGAffineTransformMakeRotation(CGFloat(rotation))
+            })
+            
+            motionManager.startGyroUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: { data, error in
+                //print("asd")
+                //print(data!.rotationRate.x)
+                //print(data!)
+                //print(self.image.frame.midX)
+                let imageCenter = self.image.center.x
+                let xData = Int((data!.rotationRate.y)*50)
+                let newImageX = imageCenter + CGFloat(xData)
+                
+                //let rotation = atan2((data?.rotationRate.x)!, (data?.rotationRate.y)!) - M_PI
+                self.image.center.x = CGFloat(newImageX)
+            })
+            
+        } else {
+            print("else")
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
